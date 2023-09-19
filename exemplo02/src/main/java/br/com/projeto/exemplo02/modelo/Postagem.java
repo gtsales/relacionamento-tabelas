98307b1c97575db90ddfc8f6169b5467ee759f51
gtsales <soutoluiz0609@gmail.com>
2023-09-19 18:15:21 -0300

    Relacionamento OneToMany criado

--- exemplo02/src/main/java/br/com/projeto/exemplo02/modelo/Postagem.java
+++ exemplo02/src/main/java/br/com/projeto/exemplo02/modelo/Postagem.java
@@ -1,9 +1,13 @@
 package br.com.projeto.exemplo02.modelo;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,4 +26,8 @@ public class Postagem {
 
     private String texto;
 
+    @OneToMany
+    @JoinColumn(name = "codigo_postagem")//Aqui diferente da relação OneToOne, a chave estrangeira é declarada no objeto 1, porém é crida no objeto many(Comentario)
+    //Ou seja, a coluna codigo_postagem foi criada na tabela comentarios do BD. Nesse relacionamento a chave estrangeira é criada na coluna many
+    private List<Comentario> comentarios; //Declara que é possível ter vários (objetos)comentarios no (objeto)post
 }
